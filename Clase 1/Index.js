7556d902c39fa45f7858c0d0fe87afb96e7a90df
A.Wisdom.G <aspiringwisdomtraining@gmail.com>
2024-07-03 18:28:50 -0600

    Clase 1 Folder

---
+++ Clase 1/Index.js
@@ -0,0 +1,15 @@
+
+console.log("Hola Mundo!")
+
+document.write("Hola Mundo")
+
+alert("Test")
+
+var Edad = prompt("Tu Edad?")
+console.log(Edad)
+
+var nombre = prompt("Ingresa Nombre")
+console.log("El Nommbre del usario es:", nombre)
+
+
+
